0d9312399c468ac8c4a85d8546e56a55d4420cc6
Артур <Artur0795@yandex.ru>
2023-07-07 19:09:28 +0300

    dz1

---
+++ dz1.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://akabab.github.io/superhero-api/api/all.json"
+resp = requests.get(url)
+heroes_list = ['Hulk', 'Captain America', 'Thanos']
+dictionaries = resp.json()
+intelligence = []
+for meaning in dictionaries:
+    result = meaning['powerstats']['intelligence']
+    if meaning['name'] in heroes_list:
+        intelligence.append(result)
+heroes_list_intelligence = dict(zip(heroes_list, intelligence))
+maximum = max(heroes_list_intelligence, key=heroes_list_intelligence.get)
+print(maximum)
